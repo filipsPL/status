f6ab551b97470ed6e3fe9039468004aa04bc9bbc
Filip Stefaniak <stefaniak@gmail.com>
2015-10-27 22:05:40 +0100

    status pages

---
+++ js/config-my.js
@@ -0,0 +1,15 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm777179445-8a3e91032bbb1dacd859e321',
+	'm777179469-6ca2877ab59335b58d772ba0',
+	'm777179450-f399c1713b1d20e823474308',
+	'm777180075-1b717a014cd53fd6c7dd5ac1',
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
+
+// the default language
+// set false to disable language support and show only english,
+// or set null to use visitor's browser language
+var __language = null;
---
+++ js/config.js
@@ -0,0 +1,16 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm777179445-8a3e91032bbb1dacd859e321',
+	'm777179472-4002c7c02d1dddce8af4a51c',
+	'm777179475-26d60baffe902288cd53ba99',
+	'm777180029-52d64e379145c803ed1aef80',
+	'm777180033-4993164347ea05aa69bb0249',
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
+
+// the default language
+// set false to disable language support and show only english,
+// or set null to use visitor's browser language
+var __language = null;
